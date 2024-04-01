8145dec87d129310b7e86bfa60e5e495344c6ac8
Jonathan <emeraldusk@gmail.com>
2024-04-01 13:35:40 -0300

    feat(mks): improvements

--- mks/main.zig
+++ mks/main.zig
@@ -1,31 +1,37 @@
 const std = @import("std");
 const fs = std.fs;
 
-const folders_to_create = [_][]const u8{
-    "scripts",
-    "scans",
-    "files",
-};
+const folders_to_create: [3][]const u8 = [3][]const u8{ "scripts", "scans", "files" };
+
+pub fn makeDir(cwd: fs.Dir, folderName: []const u8) !void {
+    try cwd.makeDir(folderName);
+}
+
+pub fn getFolderName(allocator: std.mem.Allocator) ![]const u8 {
+    const args: [][]u8 = try std.process.argsAlloc(allocator);
+
+    if (args.len < 2) {
+        std.debug.print("{s}", .{"Missing folder name \n"});
+        std.process.exit(1);
+    }
+
+    return args[1];
+}
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator: std.mem.Allocator = arena.allocator();
+    defer arena.deinit();
 
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    const folderName: []const u8 = try getFolderName(allocator);
+    const cwd: fs.Dir = fs.cwd();
+    try makeDir(cwd, folderName);
 
-    const folderName: [:0]u8 = args[1];
-    const cwd = fs.cwd();
-    try cwd.makeDir(folderName);
-
-    // Move into the new directory
-    var newDir = try cwd.openDir(folderName, .{});
+    var newDir: fs.Dir = try cwd.openDir(folderName, .{});
     defer newDir.close();
     try newDir.setAsCwd();
 
-    // Create the folders
     for (&folders_to_create) |folder| {
-        try cwd.makeDir(folder);
+        try makeDir(cwd, folder);
     }
 }
