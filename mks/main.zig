61070241073b909aaaca3734088fed7b63ec32c8
Jonathan <emeraldusk@gmail.com>
2024-04-02 13:25:57 -0300

    feat(mks): now creates readme.md

--- mks/main.zig
+++ mks/main.zig
@@ -2,12 +2,13 @@ const std = @import("std");
 const fs = std.fs;
 
 const folders_to_create: [3][]const u8 = [3][]const u8{ "scripts", "scans", "files" };
+const file_to_create: *const [9]u8 = "README.md";
 
-pub fn makeDir(cwd: fs.Dir, folderName: []const u8) !void {
+fn makeDir(cwd: fs.Dir, folderName: []const u8) !void {
     try cwd.makeDir(folderName);
 }
 
-pub fn getFolderName(allocator: std.mem.Allocator) ![]const u8 {
+fn getFolderName(allocator: std.mem.Allocator) ![]const u8 {
     const args: [][]u8 = try std.process.argsAlloc(allocator);
 
     if (args.len < 2) {
@@ -16,6 +17,10 @@ pub fn getFolderName(allocator: std.mem.Allocator) ![]const u8 {
     }
 
     return args[1];
+}
+
+fn createFile(cwd: fs.Dir, fileName: []const u8) !fs.File {
+    return try cwd.createFile(fileName, .{});
 }
 
 pub fn main() !void {
@@ -30,6 +35,9 @@ pub fn main() !void {
     var newDir: fs.Dir = try cwd.openDir(folderName, .{});
     defer newDir.close();
     try newDir.setAsCwd();
+
+    const newFile = try createFile(cwd, file_to_create);
+    defer newFile.close();
 
     for (&folders_to_create) |folder| {
         try makeDir(cwd, folder);
